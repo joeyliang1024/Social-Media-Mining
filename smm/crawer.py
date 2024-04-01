48c7b914cf1c119641c88b18e5f4de1ca1205ca9
Joey Liang <95967774+joeyliang1024@users.noreply.github.com>
2024-04-01 23:35:14 +0800

    Update crawer.py
    Finish all functions, to be tests...

--- smm/crawer.py
+++ smm/crawer.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
 
 class SingleBookProcessor:
     def __init__(self, type_name, first_page_url):
@@ -20,11 +22,11 @@ class SingleBookProcessor:
         self.sections = {}                   # ex: {"附錄1":{"section_first_page":"https://123456789"},"附錄2":{"section_first_page":"https://111222333"}}
         self.data = {}                       # not defined yet
 
-    def title_cleaning(self, origin_title):
+    def title_cleaning(self, origin_title, return_list=True):
         clean_title = re.sub(u'\u3000', ' ', origin_title).strip()
         clean_title = re.sub(r'\(.+?\)|\d+日', '', clean_title).strip()
         title_list = [item for item in clean_title.split('／')[2:] if item != '']
-        return ' '.join(title_list)
+        return title_list if return_list else ' '.join(title_list)
 
     def get_all_sections(self):
         self.driver.get(self.first_page_url)
@@ -32,7 +34,7 @@ class SingleBookProcessor:
         self.sections = {column.find_element(By.TAG_NAME, 'font').text:{"section_first_page":column.find_element(By.TAG_NAME, 'font').find_element(By.XPATH, '..').get_attribute('href')}
                          for column in self.driver.find_element(By.XPATH, '//*[@class="div3"]/table').find_elements(By.TAG_NAME, 'tr')[3:] 
                          if column.find_element(By.TAG_NAME, 'font')}
-
+        
     def click_next_page(self):
         '''
         return True if have next page, else return False
@@ -40,18 +42,35 @@ class SingleBookProcessor:
         # driver should start from the page: sections["section name"]["section_first_page"]
         # if the page title not in sections.key name, then this section is finished, return False
         # else return True 
-        #TODO
-        pass
+        try:
+            next_page_btn = self.driver.find_element(By.XPATH, '//*[@title="下一章"]').find_element(By.XPATH, '..')
+            # 檢查是否同一個書目
+            # 是：點選下一頁
+            self.driver.get(next_page_btn.get_attribute('href'))
+            book_name = self.title_cleaning(self.driver.find_element(By.XPATH, '//*[@class="gobookmark"]').text)[1]
+            # 睡一下，怕爬太快被封
+            time.sleep(random.uniform(0, 5))
+            if book_name == self.type_name:
+                return True
+            # 否：換下一本
+            else:
+                return False
+        except NoSuchElementException:
+            # 沒有下一位
+            return False
     
     def get_element_content(self):
-        '''
-        get the current page elements, our object.
-        '''
-        result_dict = {} 
-        #TODO
-        pass
-        return result_dict
-    
+        # title: <a> class="gobookmark"
+        # contexts: <div> style="text-indent:0em;padding-left:0em;"
+        try:
+            title = self.title_cleaning(self.driver.find_element(By.XPATH, '//*[@class="gobookmark"]').text, return_list=False)
+            contexts = [_.text for _ in self.driver.find_elements(By.XPATH, '//*[@style="text-indent:0em;padding-left:0em;"]')]
+            return {"Title":title,
+                    "Context":contexts} 
+        except:
+            return {"Title":"",
+                    "Context":[]} 
+
     def crawling(self, progress_bar=None):
         '''
         start crawing the book data, updata tqdm bar if finish a section.
@@ -75,7 +94,6 @@ class SingleBookProcessor:
                     pass  
             if progress_bar:
                 progress_bar.update(1)
-            time.sleep(random.uniform(0, 5))
         self.driver.close()
 
 class MutiThreadProcessor:
@@ -89,9 +107,6 @@ class MutiThreadProcessor:
         self.data = {}
     
     def get_home_page(self):
-        '''
-        finished
-        '''
         self.driver.get('http://google.com')
         googlr_search_input_area = self.driver.find_element(By.XPATH, '//*[@class="gLFyf"]')
         googlr_search_input_area.send_keys("明實錄 中央研究院歷史語言研究所")
@@ -108,9 +123,6 @@ class MutiThreadProcessor:
         self.init_url = self.driver.current_url
 
     def load_books_tree_graph(self):
-        '''
-        finished
-        '''
         try:
             msl_btn = self.driver.find_element(By.XPATH, '//*[@id="frmTitle"]/table/tbody/tr[4]/td/div/table/tbody/tr[3]/td[4]/a[2]')
             msl_btn.click()
@@ -120,9 +132,6 @@ class MutiThreadProcessor:
             print("This page has no object tee graph you need.")
 
     def get_all_craw_types(self):
-        '''
-        finished
-        '''
         not_need_books = ["版本說明", "校印本明實錄總目", "校勘記凡例"]
         book_names = [i.text for i in self.driver.find_elements(By.XPATH, '//*[@title="P.1"]') if i.text not in not_need_books]
         urls = []
@@ -131,12 +140,9 @@ class MutiThreadProcessor:
                 urls.append(element.find_element(By.TAG_NAME, 'a').get_attribute("href"))
             except:
                 pass
-        self.object_dict = {book:{"first_page_url":url} for book, url in zip(book_names, urls)}
+        self.object_url_dict = {book:{"first_page_url":url} for book, url in zip(book_names, urls)}
 
     def crawl_single_type(self, type_name, type_info):
-        '''
-        not sure yet..., but maybe done 
-        '''
         processor = SingleBookProcessor(type_name, type_info['first_page_url'])
         processor.get_all_sections()
         pbar = tqdm(total=int(processor.total_section_num), desc=type_name)
@@ -144,9 +150,6 @@ class MutiThreadProcessor:
         self.data.update(processor.data)
 
     def muti_thread_crawling(self):
-        '''
-        not sure yet..., but maybe done 
-        '''
         self.get_home_page()
         print(f"Home page url this time: {self.init_url}")
         print(f"Now driver url location is at {self.init_url}")
@@ -154,7 +157,8 @@ class MutiThreadProcessor:
         print("Book tree graph loaded.")
         print("Now getting all books tpyes:")
         self.get_all_craw_types()
-
+        for key, value in self.object_url_dict.items():
+            print(f"Book: {'　' * (6 - len(key))}{key}, Url: {value['first_page_url']}")
         with ThreadPoolExecutor() as executor:
             # Create a list of arguments for crawl_single_type
             type_names, type_infos = [], []
@@ -166,15 +170,9 @@ class MutiThreadProcessor:
                 executor.map(self.crawl_single_type, type_names, type_infos)
             except Exception as e:
                 print(f"An error occurred: {e}")
+
         self.driver.quit()
     
     def save_data(self):
-        '''
-        finished, but you can save to csv as well
-        '''
         with open(self.save_path, 'w', encoding='utf-8') as f:
             json.dump(self.data, f, ensure_ascii=False)
-
-
-    
-
