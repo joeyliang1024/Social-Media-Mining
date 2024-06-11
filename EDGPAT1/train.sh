1e62dbc2a2fefdd549cacdc38dc9a9b698d3bee0
joeyliang1024 <joeyqazwsxedc@gmail.com>
2024-06-11 16:48:34 +0800

    updata EDGPAT

---
+++ EDGPAT1/train.sh
@@ -0,0 +1,11 @@
+python main.py \
+    --gpu 0 \
+    --bs 128 \
+    --drop_out 0.5 \
+    --n_company 766 \
+    --n_field_5 20113\
+    --n_field_4 3690\
+    --n_field_3 526\
+    --n_field_2 119\
+    --n_field_1 8 \
+    > out.txt
